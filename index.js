db10681dde5cb1fb02d157d3ab00099f92df3504
hannamastyka <hanna.mastyka@outlook.com>
2023-08-27 12:02:19 +0200

    poprawione

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
 const incomesForm = document.getElementById("incomes-form");
 const incomesList = document.getElementById("incomes-list");
+const totalIncomes = document.getElementById("total-incomes");
 
 const incomes = [];
+
+function calculateTotalIncomes() {
+  const amounts = incomes.map((income) => Number(income.amount));
+  const sum = amounts.reduce((a, b) => a + b, 0);
+  totalIncomes.textContent = `Suma przychodów: ${sum} zł`;
+}
 
 function renderIncomesList() {
   incomesList.innerHTML = "";
   incomes.forEach((income) => {
     const item = document.createElement("li");
     item.textContent = `${income.title}: ${income.amount} zł`;
+    // dodac buttony
     incomesList.appendChild(item);
   });
+  calculateTotalIncomes();
 }
 
 incomesForm.addEventListener("submit", (event) => {
