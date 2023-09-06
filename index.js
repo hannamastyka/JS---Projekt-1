22224a108802ae8c3fe3e91d54d3c16b8652b99b
hannamastyka <hanna.mastyka@outlook.com>
2023-09-06 19:07:12 +0200

    poprawione

--- index.js
+++ index.js
@@ -1,9 +1,32 @@
 const incomesForm = document.getElementById("incomes-form");
 const incomesList = document.getElementById("incomes-list");
 const totalIncomes = document.getElementById("total-incomes");
+const balanceElement = document.getElementById("balance");
 
 const incomes = [];
+const expenses = [];
 
+function updateBalanceUI() {
+  const incomesSum = incomes
+    .map((income) => Number(income.amount))
+    .reduce((a, b) => a + b, 0);
+  const expensesSum = expenses
+    .map((expense) => Number(expense.amount))
+    .reduce((a, b) => a + b, 0);
+  const balance = incomesSum - expensesSum;
+
+  if (balance > 0) {
+    balanceElement.textContent = `Możesz jeszcze wydać ${balance.toFixed(
+      2
+    )} złotych`;
+  } else if (balance === 0) {
+    balanceElement.textContent = "Bilans wynosi zero";
+  } else {
+    balanceElement.textContent = `Bilans jest ujemny. Jesteś na minusie ${Math.abs(
+      balance
+    ).toFixed(2)} złotych`;
+  }
+}
 function calculateTotalIncomes() {
   const amounts = incomes.map((income) => Number(income.amount));
   const sum = amounts.reduce((a, b) => a + b, 0);
@@ -19,6 +42,7 @@ function renderIncomesList() {
     incomesList.appendChild(item);
   });
   calculateTotalIncomes();
+  updateBalanceUI();
 }
 
 incomesForm.addEventListener("submit", (event) => {
@@ -32,21 +56,11 @@ incomesForm.addEventListener("submit", (event) => {
     amount,
     id: Math.random(),
   });
+  event.target.incomeTitle.value = "";
+  event.target.incomeAmount.value = "";
 
   renderIncomesList();
 });
-function updateBalanceUI(balance) {
-  if (balance > 0) {
-    balanceElement.textContent = `Możesz jeszcze wydać ${balance.toFixed(
-      2
-    )} złotych`;
-  } else if (balance === 0) {
-    balanceElement.textContent = "Bilans wynosi zero";
-  } else {
-    balanceElement.textContent = `Bilans jest ujemny. Jesteś na minusie ${Math.abs(
-      balance
-    ).toFixed(2)} złotych`;
-  }
-}
 
-updateUI();
+updateBalanceUI();
+
